fcb21f447ad98f90f8736281b3b05b37b9ef1343
BUowo <directswapconnections@gmail.com>
2026-04-21 11:10:24 -0400

    redo and make pos more general

--- positivityCog.py
+++ positivityCog.py
@@ -51,7 +51,7 @@ class PositivityCog(commands.Cog, name="Positivity"):
             self.count_by_guild[guild_id] = 0
             self._save_state()
             await message.channel.send(
-                f"Happy Positivity Tuesday, {message.author.mention}! 🌸✨ You have been selected to make a positive comment toward a member of the server. 💖"
+                f"Happy Positivity Tuesday, {message.author.mention}! 🌸✨ You have been selected to make a positive comment about yourself, a fellow member, or anything else. 💖 <#1413696001503531039>"
             )
             return
 
